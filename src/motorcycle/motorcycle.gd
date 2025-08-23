23d32b23b8e028de36689c98880047c21a4726e6
Teddy Sauyet (surface laptop) <teddy.sauyet@gmail.com>
2025-08-23 06:04:15 -0400

    stable 4 wheeler

--- src/motorcycle/motorcycle.gd
+++ src/motorcycle/motorcycle.gd
@@ -8,4 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	engine_force += delta * 100.0
+	engine_force += delta * 0.0
