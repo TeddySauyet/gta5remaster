ee4e03a836d1d6458e8e5b2e678c69141d757da9
Teddy Sauyet (surface laptop) <teddy.sauyet@gmail.com>
2025-08-25 13:44:43 -0400

    Can stabilize motorcycle - causes sliding

--- src/motorcycle/motorcycle.gd
+++ src/motorcycle/motorcycle.gd
@@ -1,5 +1,10 @@
 extends VehicleBody3D
 
+var last_position := Vector3.ZERO
+var velocity := Vector3.ZERO
+var last_velocity := Vector3.ZERO
+var acceleration := Vector3.ZERO
+var save_n_vels := 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +13,23 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	engine_force += delta * 0.0
+	engine_force = delta * 500 * Input.get_axis("throttle_down", "throttle_up")
+
+func _physics_process(_delta: float) -> void:
+	update_acceleration()
+	set_roll()
+	pass
+
+## Call from physics process pls
+func update_acceleration() -> void:
+	velocity = position - last_position
+	last_position = position
+	acceleration *= (save_n_vels - 1)/save_n_vels
+	acceleration += (velocity - last_velocity)/save_n_vels
+	last_velocity = velocity
+
+func set_roll() -> void:
+	var desired_x_dir := transform.basis.x
+	desired_x_dir.y = 0.0
+	var angle := transform.basis.x.signed_angle_to(desired_x_dir,-transform.basis.z)
+	transform = transform.rotated_local(-transform.basis.z,angle)
