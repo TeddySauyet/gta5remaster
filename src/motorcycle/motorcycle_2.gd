8db11dffff4bb31ad567891917c642c13b901c79
Teddy Sauyet (surface laptop) <teddy.sauyet@gmail.com>
2025-09-01 15:00:52 -0400

    motorcycle v2 mvp complete

--- src/motorcycle/motorcycle_2.gd
+++ src/motorcycle/motorcycle_2.gd
@@ -3,12 +3,17 @@ class_name Motorcyle
 
 var max_speed := 60.0
 var acceleration := 10.0
-var max_angle := 45.0/180.0*PI
-var max_steering := 45.0/180.0*PI
+var max_angle := 60.0/180.0*PI
+var max_steering := 5.0/180.0*PI
 
-var speed = 0.0
+var speed := 0.0
 var steering := 0.0
 var steering_speed := 0.1
+
+var current_gear := 1
+var speed_per_gear := 8.0
+var gear_transition_threshhold := 12.0
+var gear_min_disadvantage := 0.65
 
 var time_since_on_floor = 0.0
 
@@ -17,22 +22,36 @@ func _physics_process(delta: float) -> void:
 	if steering_delta != 0:
 		steering = steering + steering_delta
 	else:
-		steering = 0.95*delta*steering
-		if steering < 0.05:
+		steering = steering - steering_speed *sign(steering) * delta
+		if abs(steering) < 2 * steering_speed * delta:
 			steering = 0
 	
-	steering = clampf(steering, -max_steering, max_steering)
+	
+	var speed_factor := 1.0 - velocity.length()/100.0
+	steering = clampf(steering, -max_steering*speed_factor, max_steering*speed_factor)
 	
 	var angle := transform.basis.y.angle_to(Vector3.UP) * signf(transform.basis.x.dot(Vector3.UP))
-	var desired_angle := steering*absf(speed)/(max_steering*max_speed) * max_angle
+	var desired_angle := -steering*absf(speed)/(max_steering*max_speed) * max_angle
 	
 	
-	transform = transform.rotated_local(transform.basis.y.normalized(), steering)
+	if time_since_on_floor < 0.2:
+		transform = transform.rotated_local(transform.basis.y.normalized(), steering)
 	#transform = transform.rotated_local(transform.basis.z, desired_angle - angle)
+	$CSGCombiner3D.transform = Transform3D.IDENTITY
+	$CSGCombiner3D.transform = $CSGCombiner3D.transform.rotated_local(-$CSGCombiner3D.transform.basis.z, desired_angle)
 	
-	print_debug(desired_angle,'|',angle)
 	var delta_speed := Input.get_axis("throttle_down", "throttle_up") * delta * acceleration
-	delta_speed = clampf(delta_speed, -speed, max_speed-speed)
+	
+	var dist_to_current_gear := speed_per_gear*current_gear - speed/current_gear
+	var gear_disadvantage := lerpf(1.0, gear_min_disadvantage,absf(dist_to_current_gear)/speed_per_gear)
+	gear_disadvantage = clampf(gear_disadvantage, gear_min_disadvantage,1.0)
+	
+	if absf(dist_to_current_gear)/gear_transition_threshhold >= 1.0:
+		current_gear -= signf(dist_to_current_gear)
+		if(current_gear <= 0):
+			current_gear = 1
+	
+	delta_speed = clampf(delta_speed*gear_disadvantage, -speed, max_speed-speed)
 	
 	speed += delta_speed
 	
@@ -41,8 +60,8 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += 9.8*Vector3.DOWN * time_since_on_floor
 		time_since_on_floor += delta
-		print_debug("hi")
 	else:
 		time_since_on_floor = 0
 		
 	move_and_slide()
+	print_debug(velocity.length(), '|', gear_disadvantage)
